zunda/heroku-buildpack-ruby

add bundler specs for windows Gemfile detect

spec/helpers/bundler_wrapper_spec.rb
@@ -1,0 +1,16 @@
+require 'spec_helper'
+
+describe "BundlerWrapper" do
+
+  after(:each) do
+    FileUtils.remove_entry_secure("tmp") if Dir.exist?("tmp")
+  end
+
+  fit "detects windows gemfiles" do
+    Hatchet::App.new("rails4_windows_mri193").in_directory do |dir|
+      @bundler = LanguagePack::Helpers::BundlerWrapper.new(gemfile_path: "./Gemfile")
+      expect(@bundler.windows_gemfile_lock?).to be_true
+    end
+  end
+end
+

sbrady/cf-buildpack-ruby

spec/helpers/bundler_wrapper_spec.rb
@@ -1,0 +1,16 @@
+require 'spec_helper'
+
+describe "BundlerWrapper" do
+
+  after(:each) do
+    FileUtils.remove_entry_secure("tmp") if Dir.exist?("tmp")
+  end
+
+  it "detects windows gemfiles" do
+    Hatchet::App.new("rails4_windows_mri193").in_directory do |dir|
+      @bundler = LanguagePack::Helpers::BundlerWrapper.new(gemfile_path: "./Gemfile")
+      expect(@bundler.windows_gemfile_lock?).to be_true
+    end
+  end
+end
+