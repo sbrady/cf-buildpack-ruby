vladiim/heroku-buildpack-ruby

[fix #171] Refactor Rake Detect and Run
Move rake task detection and running to a separate independently tested class.

Compile exits when rake tasks cannot be successfully detected with error message

lib/language_pack/rails3.rb
@@ -59,7 +59,9 @@ private
     instrument "rails3.run_assets_precompile_rake_task" do
       log("assets_precompile") do
         setup_database_url_env
-        return true unless rake_task_defined?("assets:precompile")
+
+        precompile = rake.task("assets:precompile")
+        return true unless precompile.is_defined?
 
         topic("Preparing app for Rails asset pipeline")
         if File.exists?("public/assets/manifest.yml")
@@ -72,11 +74,11 @@ private
 
         puts "Running: rake assets:precompile"
         require 'benchmark'
-        time = Benchmark.realtime { pipe("env PATH=$PATH:bin bundle exec rake assets:precompile 2>&1") }
 
-        if $?.success?
+        precompile.run
+        if precompile.success?
           log "assets_precompile", :status => "success"
-          puts "Asset precompilation completed (#{"%.2f" % time}s)"
+          puts "Asset precompilation completed (#{"%.2f" % precompile.time}s)"
         else
           log "assets_precompile", :status => "failure"
           error "Precompiling assets failed."

lib/language_pack/rails4.rb
@@ -73,7 +73,8 @@ WARNING
       log("assets_precompile") do
         setup_database_url_env
 
-        return true unless rake_task_defined?("assets:precompile")
+        precompile = rake.task("assets:precompile")
+        return true unless precompile.is_defined?
 
         if Dir.glob('public/assets/manifest-*.json').any?
           puts "Detected manifest file, assuming assets were compiled locally"
@@ -87,13 +88,11 @@ WARNING
         @cache.load public_assets_folder
         @cache.load default_assets_cache
 
-        puts "Running: rake assets:precompile"
-        require 'benchmark'
-        time = Benchmark.realtime { pipe("env PATH=$PATH:bin bundle exec rake assets:precompile 2>&1 > /dev/null") }
+        precompile.run
 
-        if $?.success?
+        if precompile.success?
           log "assets_precompile", :status => "success"
-          puts "Asset precompilation completed (#{"%.2f" % time}s)"
+          puts "Asset precompilation completed (#{"%.2f" % precompile.time}s)"
 
           puts "Cleaning assets"
           pipe "env PATH=$PATH:bin bundle exec rake assets:clean 2>& 1"

lib/language_pack/ruby.rb
@@ -387,8 +387,8 @@ WARNING
     instrument 'ruby.install_language_pack_gems' do
       FileUtils.mkdir_p(slug_vendor_base)
       Dir.chdir(slug_vendor_base) do |dir|
-        gems.each do |gem|
-          @fetchers[:buildpack].fetch_untar("#{gem}.tgz")
+        gems.each do |g|
+          @fetchers[:buildpack].fetch_untar("#{g}.tgz")
         end
         Dir["bin/*"].each {|path| run("chmod 755 #{path}") }
       end
@@ -652,13 +652,8 @@ params = CGI.parse(uri.query || "")
     bundle.specs.map(&:name).include?(gem)
   end
 
-  # detects if a rake task is defined in the app
-  # @param [String] the task in question
-  # @return [Boolean] true if the rake task is defined in the app
-  def rake_task_defined?(task)
-    instrument "ruby.rake_task_defined" do
-      run("env PATH=$PATH bundle exec rake #{task} --dry-run") && $?.success?
-    end
+  def rake
+    @rake ||= LanguagePack::Helpers::RakeRunner.new(gem_is_bundled?("rake")).load_rake_tasks!
   end
 
   # executes the block with GIT_DIR environment variable removed since it can mess with the current working directory git thinks it's in
@@ -684,13 +679,17 @@ params = CGI.parse(uri.query || "")
 
   def run_assets_precompile_rake_task
     instrument 'ruby.run_assets_precompile_rake_task' do
-      if rake_task_defined?("assets:precompile")
 
-        topic "Running: rake assets:precompile"
-        time = Benchmark.realtime { pipe("env PATH=$PATH:bin bundle exec rake assets:precompile 2>&1") }
-        if $?.success?
-          puts "Asset precompilation completed (#{"%.2f" % time}s)"
-        end
+      precompile = rake.task("assets:precompile")
+      return true unless precompile.is_defined?
+
+      topic "Running: rake assets:precompile"
+      precompile.run
+      if precompile.success?
+        puts "Asset precompilation completed (#{"%.2f" % precompile.time}s)"
+      else
+        log "assets_precompile", :status => "failure"
+        error "Precompiling assets failed."
       end
     end
   end

sbrady/cf-buildpack-ruby

lib/language_pack/rails3.rb
@@ -59,7 +59,9 @@ private
     instrument "rails3.run_assets_precompile_rake_task" do
       log("assets_precompile") do
         setup_database_url_env
-        return true unless rake_task_defined?("assets:precompile")
+
+        precompile = rake.task("assets:precompile")
+        return true unless precompile.is_defined?
 
         topic("Preparing app for Rails asset pipeline")
         if File.exists?("public/assets/manifest.yml")
@@ -72,11 +74,11 @@ private
 
         puts "Running: rake assets:precompile"
         require 'benchmark'
-        time = Benchmark.realtime { pipe("env PATH=$PATH:bin bundle exec rake assets:precompile 2>&1") }
 
-        if $?.success?
+        precompile.invoke
+        if precompile.success?
           log "assets_precompile", :status => "success"
-          puts "Asset precompilation completed (#{"%.2f" % time}s)"
+          puts "Asset precompilation completed (#{"%.2f" % precompile.time}s)"
         else
           log "assets_precompile", :status => "failure"
           error "Precompiling assets failed."

lib/language_pack/rails4.rb
@@ -73,7 +73,8 @@ WARNING
       log("assets_precompile") do
         setup_database_url_env
 
-        return true unless rake_task_defined?("assets:precompile")
+        precompile = rake.task("assets:precompile")
+        return true unless precompile.is_defined?
 
         if Dir.glob('public/assets/manifest-*.json').any?
           puts "Detected manifest file, assuming assets were compiled locally"
@@ -87,13 +88,11 @@ WARNING
         @cache.load public_assets_folder
         @cache.load default_assets_cache
 
-        puts "Running: rake assets:precompile"
-        require 'benchmark'
-        time = Benchmark.realtime { pipe("env PATH=$PATH:bin bundle exec rake assets:precompile 2>&1 > /dev/null") }
+        precompile.invoke
 
-        if $?.success?
+        if precompile.success?
           log "assets_precompile", :status => "success"
-          puts "Asset precompilation completed (#{"%.2f" % time}s)"
+          puts "Asset precompilation completed (#{"%.2f" % precompile.time}s)"
 
           puts "Cleaning assets"
           pipe "env PATH=$PATH:bin bundle exec rake assets:clean 2>& 1"

lib/language_pack/ruby.rb
@@ -387,8 +387,8 @@ WARNING
     instrument 'ruby.install_language_pack_gems' do
       FileUtils.mkdir_p(slug_vendor_base)
       Dir.chdir(slug_vendor_base) do |dir|
-        gems.each do |gem|
-          @fetchers[:buildpack].fetch_untar("#{gem}.tgz")
+        gems.each do |g|
+          @fetchers[:buildpack].fetch_untar("#{g}.tgz")
         end
         Dir["bin/*"].each {|path| run("chmod 755 #{path}") }
       end
@@ -652,13 +652,8 @@ params = CGI.parse(uri.query || "")
     bundle.specs.map(&:name).include?(gem)
   end
 
-  # detects if a rake task is defined in the app
-  # @param [String] the task in question
-  # @return [Boolean] true if the rake task is defined in the app
-  def rake_task_defined?(task)
-    instrument "ruby.rake_task_defined" do
-      run("env PATH=$PATH bundle exec rake #{task} --dry-run") && $?.success?
-    end
+  def rake
+    @rake ||= LanguagePack::Helpers::RakeRunner.new(gem_is_bundled?("rake")).load_rake_tasks!
   end
 
   # executes the block with GIT_DIR environment variable removed since it can mess with the current working directory git thinks it's in
@@ -684,13 +679,17 @@ params = CGI.parse(uri.query || "")
 
   def run_assets_precompile_rake_task
     instrument 'ruby.run_assets_precompile_rake_task' do
-      if rake_task_defined?("assets:precompile")
 
-        topic "Running: rake assets:precompile"
-        time = Benchmark.realtime { pipe("env PATH=$PATH:bin bundle exec rake assets:precompile 2>&1") }
-        if $?.success?
-          puts "Asset precompilation completed (#{"%.2f" % time}s)"
-        end
+      precompile = rake.task("assets:precompile")
+      return true unless precompile.is_defined?
+
+      topic "Running: rake assets:precompile"
+      precompile.invoke
+      if precompile.success?
+        puts "Asset precompilation completed (#{"%.2f" % precompile.time}s)"
+      else
+        log "assets_precompile", :status => "failure"
+        error "Precompiling assets failed."
       end
     end
   end