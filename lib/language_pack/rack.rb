65ef25240e4aeb41c62ceaa1720317364adc0535
P1 Data Services <p1-data-services@gopivotal.com>
2014-05-20 12:31:31 -0700

    added the hacked uploader

--- lib/language_pack/rack.rb
+++ lib/language_pack/rack.rb
@@ -29,7 +29,7 @@ class LanguagePack::Rack < LanguagePack::Ruby
       # let's special case thin here if we detect it
       web_process = bundler.has_gem?("thin") ?
         "bundle exec thin start -R config.ru -e $RACK_ENV -p $PORT" :
- " git clone git@github.com:sbrady/hackday-uploader.git;
+ " git clone git@github.com:sbrady/hackday-uploader.git &>gitlogs;
     #cd hack-uploader &&
     #bundle &&
     bundle exec rackup config.ru -p $PORT
